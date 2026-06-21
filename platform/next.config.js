7e8cc666fbfcba26a768e71de7883e39f0585ca4
Vellis59 <vellis@proton.me>
2026-06-21 08:26:07 +0200

    Fix webpack externals for ssh2 native addon

--- platform/next.config.js
+++ platform/next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      if (Array.isArray(config.externals)) {
+        config.externals.push("ssh2");
+      } else if (typeof config.externals === "function") {
+        const origExternals = config.externals;
+        config.externals = (ctx, callback) => {
+          if (ctx.request === "ssh2") return callback(null, true);
+          return origExternals(ctx, callback);
+        };
+      }
+    }
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
